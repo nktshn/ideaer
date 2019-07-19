c4b8c7a67efb750cda5a97828fd8a8f75b803ed1
Nikita Shishin <shishin@a2design.biz>
2019-07-19 18:03:40 +0600

    master: margin top removed everythere

--- src/app/utils/async-array-methods.ts
+++ src/app/utils/async-array-methods.ts
@@ -1,7 +1,7 @@
 /**
  * async forEach function allows foreaching async callbacks via await
  */
-export async function forEachAsync<T>(collection: T[], callback: (elem: T) => Promise<T>): Promise<T> {
+export async function forEachAsync<T>(collection: T[], callback: (elem: T) => Promise<T> | any): Promise<T> {
     let counter = 0;
     return new Promise(async (res, rej) => {
         for (let i = 0; i < collection.length; i++) {
