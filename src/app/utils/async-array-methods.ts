aa581040d3c9ec7850cb3bd819ef4ff6bc3ae907
Nikita Shishin <shishin@a2design.biz>
2019-07-19 17:52:46 +0600

    master: utils -> async foreach

---
+++ src/app/utils/async-array-methods.ts
@@ -0,0 +1,16 @@
+/**
+ * async forEach function allows foreaching async callbacks via await
+ */
+export async function forEachAsync<T>(collection: T[], callback: (elem: T) => Promise<T>): Promise<T> {
+    let counter = 0;
+    return new Promise(async (res, rej) => {
+        for (let i = 0; i < collection.length; i++) {
+            const element = collection[i];
+            await callback(element);
+            counter++;
+            if (counter >= collection.length) {
+                res(element);
+            }
+        }
+    })
+}
