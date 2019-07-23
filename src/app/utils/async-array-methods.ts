d0cd5c5e8e0afd4ef0a64f9ae0b07028e96542e0
Nikita Shishin <shishin@a2design.biz>
2019-07-23 14:52:04 +0600

    master: async map func fix

--- src/app/utils/async-array-methods.ts
+++ src/app/utils/async-array-methods.ts
@@ -1,17 +1,14 @@
 /**
  * async map function allows mapping async callbacks via await
  */
-export async function mapAsync<T>(collection: T[], callback: (elem: T) => Promise<T> | any): Promise<T[]> {
-    let counter = 0;
-    let result: T[] = [];
+export async function mapAsync<T>(collection: T[], asyncCallback: (elem: T) => Promise<T> | any): Promise<T[]> {
+    let result: T[] = new Array(collection.length);
     return new Promise(async (res, rej) => {
-        for (let i = 0; i < collection.length; i++) {
+        const l = collection.length;
+        let i = 0;
+        for (i; i < l; i++) {
             const element = collection[i];
-            result.push(await callback(element));
-            counter++;
-            // if (counter >= collection.length) {
-
-            // }
+            result.push(await asyncCallback(element));
         }
         res(result);
     })
